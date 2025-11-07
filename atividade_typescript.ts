49f21c1ea39f7a18ca17ff069fcc811a4e02dad6
Giovana <159328874+GiovanaCadoni@users.noreply.github.com>
2025-11-07 14:53:56 -0300

    Add files via upload

---
+++ atividade_typescript.ts
@@ -0,0 +1,10 @@
+function fatores(multiplicador: number = 10, multiplicado: number = 5): number {
+    return multiplicador * multiplicado;
+}
+
+function dizOla (nome: string = 'Giovana') {
+    return `${nome} diz oi`
+}
+
+console.log(fatores)
+console.log(dizOla)
